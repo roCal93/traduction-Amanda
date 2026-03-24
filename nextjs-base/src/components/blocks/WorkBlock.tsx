d5fe39948bcbf221c47e89c50e486a0d24f2afab
Romain Calmelet <romaincalmelet@gmail.com>
2026-03-24 12:14:16 +0100

    workBlock

--- nextjs-base/src/components/blocks/WorkBlock.tsx
+++ nextjs-base/src/components/blocks/WorkBlock.tsx
@@ -118,9 +118,9 @@ const WorkBlock = ({
   }, [workItems, selectedCategory])
 
   const columnClasses = {
-    '2': 'grid-cols-2 md:grid-cols-2',
-    '3': 'grid-cols-2 md:grid-cols-2 lg:grid-cols-3',
-    '4': 'grid-cols-2 md:grid-cols-2 lg:grid-cols-4',
+    '2': 'grid-cols-1 md:grid-cols-2',
+    '3': 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3',
+    '4': 'grid-cols-1 md:grid-cols-2 lg:grid-cols-4',
   }
 
   if (loading) {
